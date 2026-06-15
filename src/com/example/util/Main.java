4954a8a6b9a26240908b41dbb3a860e4487599d4
Semen Verbitsky <semenverbitsky@gmail.com>
2026-06-15 19:59:34 +0300

    update main

--- src/com/example/util/Main.java
+++ src/com/example/util/Main.java
@@ -2,6 +2,6 @@ package com.example.util;
 
 public class Main {
     static void main(String[] args) {
-        System.out.println(com.example.util.StringUtil.reverseLetters("J^a$v@a"));
+        System.out.println(com.example.util.StringUtil.reverseLetters("!J^a$v@a"));
     }
 }
